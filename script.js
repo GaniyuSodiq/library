d7c7a0e0849b8d5c32f7b7fb472516e75bff8016
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-06-17 07:02:12 +0100

    Test constructor

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info() = function(){
+        return `${this.title} by ${this.author}, ${this.pages}, 
+        ${this.read === true ? "I have read it" : "not read yet"} `
+    }
+}
+
+const theBbok = new Book("Ogbori Ode", "Ka kan fo", 187, true)
+
+console.log(theBbok.info())
