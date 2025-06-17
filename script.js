4660e0f93a31d2c35aa445e9da71258c9620288a
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-06-17 09:13:47 +0100

    Loop through the array to display each book

--- script.js
+++ script.js
@@ -16,10 +16,12 @@
 
 // ASSIGNMENT
 
+// array to store each book object
 const myLibrary = []
 
-function Book(title, author, pages, read){
-    if (!new.target){
+// constructor for each book template
+function Book(title, author, pages, read) {
+    if (!new.target) {
         throw Error("You must use the 'new' keyword while to create the object")
     }
     this.title = title
@@ -27,12 +29,13 @@ function Book(title, author, pages, read){
     this.pages = pages
     this.read = read
     this.uuid = self.crypto.randomUUID()
-    this.info = function(){
+    this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read === true ? "I have read it" : "not read yet"} and its UUID is ${this.uuid}`
     }
 }
 
-function addBookToLibrary(title, author, pages, read){
+// fn to create a book obj and add it to the array
+function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read)
     myLibrary.push(newBook)
 }
@@ -40,7 +43,16 @@ function addBookToLibrary(title, author, pages, read){
 addBookToLibrary("Ogbori Ode", "Ka kan fo", 187, false)
 addBookToLibrary("Think and Grow Rich", "Tom Hill", 302, true)
 addBookToLibrary("Mindset", "Katherine", 504, false)
-console.log(myLibrary)
-console.log(myLibrary[1].info())
+// console.log(myLibrary)
+// console.log(myLibrary[1].info())
 // let uuid = self.crypto.randomUUID();
 // console.log(uuid); // for example "36b8f84d-df4e-4d49-b662-bcde71a8764f"
+
+// fn to display each book on the page
+function displayBook() {
+    myLibrary.forEach(function(book){
+        console.log(book)
+    })
+}
+
+displayBook()
