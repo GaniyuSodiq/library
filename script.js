3516ecfa786ba59db56d10205e799fa9a01ab88b
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-06-17 07:19:46 +0100

    Start the assignment

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// CLASS EXERCISE
+// function Book(title, author, pages, read){
+//     this.title = title
+//     this.author = author
+//     this.pages = pages
+//     this.read = read
+//     this.info = function(){
+//         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read === true ? "I have read it" : "not read yet"} `
+//     }
+// }
+
+// const theBbok = new Book("Ogbori Ode", "Ka kan fo", 187, false)
+
+// console.log(theBbok.info()) // Ogbori Ode by Ka kan fo, 187 pages, not read yet 
+
+
+// ASSIGNMENT
+
+const myLibrary = []
+
 function Book(title, author, pages, read){
     this.title = title
     this.author = author
@@ -8,6 +28,7 @@ function Book(title, author, pages, read){
     }
 }
 
-const theBbok = new Book("Ogbori Ode", "Ka kan fo", 187, false)
-
-console.log(theBbok.info())
+function addBookToLibrary(){
+    const newBook = new Book("Ogbori Ode", "Ka kan fo", 187, false)
+    myLibrary.push(newBook)
+}
